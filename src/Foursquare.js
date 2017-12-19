51a145905ed6746a14dd2dab46cd9fb97dcdc2fc
Sandeep Danta <sandeep.danta@gmail.com>
2017-12-19 16:48:12 +0530

    online should be more powerful

--- src/Foursquare.js
+++ src/Foursquare.js
@@ -1,7 +1,7 @@
 export default class Foursquare {
     exploreVenues(opts) {
         opts = opts || {};
-        const requestEndpoint = `https://api.foursquare.com/v2/venues/search?categoryId=${opts.categoryId || "4bf58dd8d48988d143941735"}&near=${opts.city || "Vancouver"}&radius=${opts.radius || 10000}&limit=${opts.maxResults || 15}&intent=browse&oauth_token=RY3HHWCB2P34AEOBHVWCKM0SQXLXU10GTBBUC4EX1B115EGG&v=20171209`;
+        const requestEndpoint = `https://api.foursquare.com/v2/venues/search?categoryId=${opts.categoryId || "4bf58dd8d48988d143941735"}&near=${opts.city || "Vancouver"}&radius=${opts.radius || 10000}&limit=${opts.maxResults || 50}&intent=browse&oauth_token=RY3HHWCB2P34AEOBHVWCKM0SQXLXU10GTBBUC4EX1B115EGG&v=20171209`;
 
         return fetch(requestEndpoint)
             .then(function (response) {
